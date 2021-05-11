455a7003b81a93c7e747279d6b72f1ea45289468
Patryck <PatryckG@outlook.com>
2021-05-11 17:21:04 -0400

    Update studio.js

--- pages/api/prisma/studio.js
+++ pages/api/prisma/studio.js
@@ -3,12 +3,8 @@
 // @ts-nocheck
 import { join, resolve } from "path";
 import { default as StudioHandler } from "@prisma/studio-vercel";
-
-// Only needed for this example. This allows `@vercel/nft` to determine that this file is in use (and so does not need to be pruned).
-// If you use Postgres / MySQL, you will instead use a connection string, and will not have a DB file, and so this can be removed.
-join(process.cwd(), "prisma/dev.db");
-
+import "../../../pr";
 // Forward the Request & Response objects to `StudioHandler`. You should configure YOUR `schemaPath` here.
 export default StudioHandler({
-	schemaPath: resolve(__dirname, "../../prisma/schema.prisma"),
+	schemaPath: resolve(__dirname, "../../../prisma/schema.prisma"),
 });
