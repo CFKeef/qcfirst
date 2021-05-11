d6cb8a02f4c377180b53539ca0837568fecd927c
Patryck <PatryckG@outlook.com>
2021-05-11 17:22:42 -0400

    Update studio.js

--- pages/api/prisma/studio.js
+++ pages/api/prisma/studio.js
@@ -3,8 +3,8 @@
 // @ts-nocheck
 import { join, resolve } from "path";
 import { default as StudioHandler } from "@prisma/studio-vercel";
-import "../../../pr";
+
 // Forward the Request & Response objects to `StudioHandler`. You should configure YOUR `schemaPath` here.
 export default StudioHandler({
-	schemaPath: resolve(__dirname, "../../../prisma/schema.prisma"),
+	schemaPath: resolve(__dirname, "../../prisma/schema.prisma"),
 });
