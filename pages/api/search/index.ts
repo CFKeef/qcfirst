f34d4c979db5fdcd22c8f9a86e17c0b8a1ddad33
Patryck <PatryckG@outlook.com>
2021-05-22 17:16:33 -0400

    Update index.ts

--- pages/api/search/index.ts
+++ pages/api/search/index.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import nc from "next-connect";
 import { NextApiResponse } from "next";
 import { AuthorizedRequest } from "../../../types/util";
