8f795896f81f0f3892c21293459501f36976628d
Patryck <PatryckG@outlook.com>
2021-05-25 16:59:00 -0400

    Update prisma.ts

--- util/prisma.ts
+++ util/prisma.ts
@@ -14,15 +14,11 @@ declare global {
 let prisma: PrismaClient;
 
 if (typeof window === "undefined") {
-	if (process.env.NODE_ENV === "production") {
-		prisma = new PrismaClient();
-	} else {
-		if (!global.prisma) {
-			global.prisma = new PrismaClient();
-		}
-
-		prisma = global.prisma;
+	if (!global.prisma) {
+		global.prisma = new PrismaClient();
 	}
+
+	prisma = global.prisma;
 }
 
 export default prisma;
